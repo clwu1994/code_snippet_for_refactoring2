f30497ff4beae3b90d62e26eb2a1028698f2cadf
unknown <1445860318@qq.com>
2019-03-23 11:16:11 +0800

    添加了戏剧演出团REDEME.md需求描述文件

---
+++ theatrical/version0.0.1/statement.js
@@ -0,0 +1,34 @@
+function statement(invoice, plays) {
+  let totalAmount = 0;
+  let valumeCredits = 0;
+  let result = `Statement for ${invoice.customer}\n`;
+  const format = new Intl.NumberFormat("en-US", {
+    style: "currency",
+    currency: "USD",
+    minimumFractionDigits: 2
+  }).format;
+
+  for (let perf of invoice.performances) {
+    const play = plays[perf.playID];
+    let thisAmount = 0;
+    switch (play.type) {
+      case "tragedy":
+        thisAmount = 40000;
+        if (perf.audience > 30) {
+          thisAmount += 1000 * (perf.audience - 30);
+        }
+        break;
+      case "comedy":
+        thisAmount = 30000;
+        if (perf.audience > 20) {
+          thisAmount += 10000 + 500 * (perf.audience - 20);
+        }
+        thisAmount += 300 * perf.audience;
+        break;
+      default:
+        throw new Error(`unknow type: ${play.type}`);
+    }
+    // 添加积分
+    valumeCredits += Math.max(perf.audience - 30, 0);
+  }
+}
