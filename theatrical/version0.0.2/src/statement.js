ee7d1c8a5ff45056ca770dd7122e62df186617bb
unknown <1445860318@qq.com>
2019-03-24 11:03:20 +0800

    拆分计算阶段与格式化阶段

--- theatrical/version0.0.2/src/statement.js
+++ theatrical/version0.0.2/src/statement.js
@@ -1,72 +1,25 @@
 const invoices = require("../db/invoices.json");
 const plays = require("../db/plays.json");
-
+const createStatementData = require("./createStatementData");
 function statement(invoice, plays) {
-  let result = `Statement for ${invoice.customer}\n`;
-  for (let perf of invoice.performances) {
-    // 订单打印信息
-    result += ` ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  const statementData = createStatementData(invoice, plays);
+  return renderPlainText(statementData);
+}
+
+function renderPlainText(data) {
+  let result = `Statement for ${data.customer}\n`;
+  for (let perf of data.performances) {
+    result += ` ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
-  result += `Amount owed is ${usd(totalAmount())}\n`;
-  result += `You earned ${totalVolumeCredits()} credits\n`;
+  result += `Amount owed is ${usd(data.totalAmount)}\n`;
+  result += `You earned ${data.totalVolumeCredits} credits\n`;
   return result;
-
-  function totalAmount() {
-    let totalAmount = 0;
-    for (let perf of invoice.performances) {
-      totalAmount += amountFor(perf);
-    }
-    return totalAmount;
-  }
-
-  function totalVolumeCredits() {
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-    return volumeCredits;
-  }
-
   function usd(aNumber) {
     return new Intl.NumberFormat("en-US", {
       style: "currency",
       currency: "USD",
       minimumFractionDigits: 2
     }).format(aNumber / 100);
-  }
-
-  function volumeCreditsFor(perf) {
-    let result = 0;
-    result += Math.max(perf.audience - 30, 0);
-    if ("comedy" === playFor(perf).type)
-      result += Math.floor(perf.audience / 5);
-    return result;
-  }
-
-  function playFor(perf) {
-    return plays[perf.playID];
-  }
-
-  function amountFor(aPerformance) {
-    let thisAmount = 0;
-    switch (playFor(aPerformance).type) {
-      case "tragedy":
-        thisAmount = 40000;
-        if (aPerformance.audience > 30) {
-          thisAmount += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        thisAmount = 30000;
-        if (aPerformance.audience > 20) {
-          thisAmount += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        thisAmount += 300 * aPerformance.audience;
-        break;
-      default:
-        throw new Error(`unknow type: ${playFor(aPerformance).type}`);
-    }
-    return thisAmount;
   }
 }
 
